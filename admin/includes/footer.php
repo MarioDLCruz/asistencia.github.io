12503014480257dbbd19cad80aeb8b0efcc4d8b2
MarioDLCruz <19151667@aguascalientes.tecnm.mx>
2023-10-25 18:56:15 -0600

    se agrega

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>By <a href="https://www.facebook.com/people/Guns-N-Tracks/100090367312338/">Guns´n Tracks</a></b>
+    </div>
+    <strong>Copyright &copy; 2023 Control de Asistencia Guns´n Tracks </strong>
+</footer>
